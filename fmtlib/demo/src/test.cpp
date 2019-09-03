da5d71d5397dd4325a0bb7640cfab408554c6e6b
MrUnbreakable <13812031661@163.com>
2019-09-03 14:30:36 +0800

    modify fmtlib

---
+++ fmtlib/demo/src/test.cpp
@@ -0,0 +1,34 @@
+#include "format.h"
+#include "fmtchrono.h"
+//#include "fmt/core.h"
+//#include "fmt/compile.h"
+#include "fmtprintf.h"
+//#include "fmt/posix.h"
+//#include "fmt/ranges.h"
+#include "fmtcolor.h"
+#include <iostream>
+#include <ctime>
+int main()
+{
+    std::string ns = "world";
+    fmt::printf("Hello,%s\n",ns);
+    
+    fmt::print("Hello, {}!\n", "world");  // Python-like format string syntax
+    
+    std::string s = fmt::format("I'd rather be {1} than {0}.", "right", "happy");
+    std::cout << s << std::endl;
+    fmt::print(stderr, "Don't {}!\n", "panic");
+    fmt::print("Elapsed time: {s:.2f} seconds\n", fmt::arg("s", 1.23));
+    std::vector<int> v = {1, 2, 3};
+    fmt::print("{}\n", fmt::join(v, ", "));
+    fmt::memory_buffer buf;
+    fmt::format_to(buf, "{}", 42);
+    fmt::format_to(buf, "{:x}", 42); // 十六进制表示
+    std::cout << buf.data() << std::endl;
+    std::time_t t = std::time(nullptr);
+    fmt::print("The date is {:%Y-%m-%d}.\n", *std::localtime(&t));    
+    fmt::print(fg(fmt::color::red),"Elapsed time: {0:.2f} seconds\n", 1.23);
+    std::string message = fmt::sprintf("The answer is %d", 42);
+    std::cout << message << std::endl;
+}
+
